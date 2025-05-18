a7f848994a73bd10fc4be2861dd5ab3ee3a15000
The Infinity's <the.infinity.s.infinity@gmail.com>
2025-05-19 08:31:24 +0900

    asdf

---
+++ src/app/music/[music_id]/components/images.tsx
@@ -0,0 +1,72 @@
+export function PlayButton() {
+  return <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M6 4H10V20H6V4Z" fill="currentColor" />
+    <path d="M14 4H18V20H14V4Z" fill="currentColor" />
+  </svg>;
+}
+
+export function StopButton() {
+  return <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M8 5V19L19 12L8 5Z" fill="currentColor" />
+  </svg>;
+}
+export function PreviousMusic() {
+  return <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M16 6L6 12L16 18V6Z" fill="currentColor" />
+    <path d="M18 6V18H20V6H18Z" fill="currentColor" />
+  </svg>;
+}
+export function NextMusic() {
+  return <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M8 6L18 12L8 18V6Z" fill="currentColor" />
+    <path d="M4 6H6V18H4V6Z" fill="currentColor" />
+  </svg>;
+}
+export function SkipToBack() {
+  return <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M11 6L5 12L11 18V6Z" fill="currentColor" />
+    <path d="M19 6L13 12L19 18V6Z" fill="currentColor" />
+  </svg>;
+}
+export function SkipToForward() {
+  return <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M13 6L19 12L13 18V6Z" fill="currentColor" />
+    <path d="M5 6L11 12L5 18V6Z" fill="currentColor" />
+  </svg>;
+}
