16ca068f9a190106d62922e961c94696c08f1a52
The Infinitys <the.infinity.s.infinity@gmail.com>
2026-07-05 14:10:37 +0900

    feat: updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig, fontProviders } from "astro/config";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
+import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   fonts: [
@@ -32,7 +33,7 @@ export default defineConfig({
   },
 
   // 統合（integrations）にまとめる
-  integrations: [react()],
+  integrations: [react(), mdx()],
   vite: {
     plugins: [tailwindcss()], // Viteプラグインとして指定
   },
---
+++ src/content.config.ts
@@ -0,0 +1,40 @@
+import { glob } from "astro/loaders";
+import { defineCollection } from "astro:content";
+import { z } from "astro/zod";
+import path from "node:path";
+import fs from "node:fs";
+
+const articles = defineCollection({
+  loader: {
+    name: "article-loader",
+    load: async (context) => {
+      // 1. 元のglobローダーを実行してデータを取得
+      // ここで指定する loader の base はプロジェクトルート基準です
+      const files = await glob({
+        pattern: "**/[^_]*.mdx",
+        base: "./src/contents/articles",
+      }).load(context);
+
+      // 2. context.store を使ってデータを直接操作する
+      // context.store は内部的にデータのキーと値を持つオブジェクトです
+      for (const entry of context.store.values()) {
+        const dirPath = path.join("src/contents/articles", path.dirname(entry.id));
+
+        const folderFiles = fs.existsSync(dirPath) ? fs.readdirSync(dirPath) : [];
+        const match = folderFiles.find((file) => file.startsWith("preview."));
+
+        if (match) {
+          // entry.data を直接書き換える
+          entry.data.previewImage = `./${path.join(dirPath, match)}`;
+        }
+      }
+    },
+  },
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      previewImage: image().optional(),
+    }),
+});
+
+export const collections = { articles };
